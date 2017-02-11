81e9283c2117419134069454622a5e01b0df0bb5
羽鳥　広大 <koudai.tori@icloud.com>
2017-02-12 06:01:38 +0900

    set column in comment table

--- db/migrate/20170211175515_create_comments.rb
+++ db/migrate/20170211175515_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.body :text
+      t.image :string
+      t.group_id :integer
+      t.user_id :integer
 
       t.timestamps null: false
     end
