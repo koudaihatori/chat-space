5d2e73fc4604f8f98dd33a3f4f70f22acb19f98c
羽鳥　広大 <koudai.tori@icloud.com>
2017-03-21 16:40:24 +0900

    form_for導入

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,15 @@
 class GroupsController < ApplicationController
-  def edit
-  end
+
 
   def new
+    @group = Group.new
+  end
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+
+  def create
+    @group = Group.create(name: group_params[:name])
   end
 end
