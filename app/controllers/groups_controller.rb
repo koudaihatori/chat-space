76da2e854eaf2c7bb80f5dd307c26c35a9de9a4b
羽鳥　広大 <koudai.tori@icloud.com>
2017-03-30 20:54:28 +0900

    編集機能追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,13 +10,25 @@ class GroupsController < ApplicationController
 
 
   def create
-    @group = Group.create(group_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: "グループが作成されました"
     else
       flash.now[:alert] = "グループ作成に失敗しました"
       redirect_to controller: :groups, action: :new
+    end
   end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: "グループが編集されました"
+    else
+      flash.now[:alert] = "グループの編集に失敗しました"
+      redirect_to controller: :groups, action: :edit
+    end
+  end
+
 
   private
 
