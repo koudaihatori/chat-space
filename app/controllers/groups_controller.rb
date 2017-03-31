964ba69f039772cc1f3d2c6b7a2a9f0349683972
羽鳥　広大 <koudai.tori@icloud.com>
2017-03-31 21:24:40 +0900

    privateに@groups追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,11 @@
 class GroupsController < ApplicationController
 
+  before_action :set_group, only: [:edit, :update]
 
   def new
     @group = Group.new
   end
   def edit
-    @group = Group.find(params[:id])
   end
 
 
@@ -15,17 +15,16 @@ class GroupsController < ApplicationController
       redirect_to root_path, notice: "グループが作成されました"
     else
       flash.now[:alert] = "グループ作成に失敗しました"
-      redirect_to controller: :groups, action: :new
+      render action: :new
     end
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to root_path, notice: "グループが編集されました"
     else
       flash.now[:alert] = "グループの編集に失敗しました"
-      redirect_to controller: :groups, action: :edit
+      render action: :edit
     end
   end
 
@@ -34,5 +33,9 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:name, user_ids: [] << current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
