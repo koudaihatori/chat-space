2895172d8bc8aa06d2deb2e00346fd2a50707028
羽鳥　広大 <koudai.tori@icloud.com>
2017-04-10 14:42:50 +0900

    install nest routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "comments#index"
-  resources :groups, only: [:new, :edit, :update, :destroy, :create]
-  resources :comments, only: [:index, :create]
+  root "groups#index"
+  resources :groups, only: [:index, :new, :edit, :update, :destroy, :create] do
+    resources :comments, only: [:index, :create]
+  end
 end
