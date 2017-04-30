b1448a38ab0281c0cd9723e4f3288a81011f2bff
羽鳥　広大 <koudai.tori@icloud.com>
2017-04-30 20:11:42 +0900

    add factories

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :group do
-    name  { Faker::Name.name}
+    name { Faker::Lorem.sentence }
   end
 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+FactoryGirl.define do
+
+  factory :user do
+    password = Faker::Internet.password(min_length = 8)
+
+    name                  Faker::Name.name
+    email                 Faker::Internet.email
+    password              password
+    password_confirmation password
+
+    after(:create) do |user|
+      user.groups << create(:group)
+    end
+  end
+
+end
