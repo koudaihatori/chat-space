47ce1f6d4113d1a790ec99b59ffb4e8e10b57088
羽鳥　広大 <koudai.tori@icloud.com>
2017-05-11 21:48:26 +0900

    delete binding pry

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,18 +6,19 @@ require 'rails_helper'
     let(:group) { create(:group) }
     let(:comment) { create(:comment) }
     let(:comments) { create_list(:comment, 5, group_id: group.id)}
-    before do
-      user = create(:user)
-      login_user user
-      # controller_macros.rb内のlogin_userメソッドを呼び出し
-    end
+
 
     describe 'GET #index' do
+      before do
+        user = create(:user)
+        login_user user
+        # controller_macros.rb内のlogin_userメソッドを呼び出し
+      end
+
       before do
         get :index, params: { group_id: group.id }
       end
       it "populates an array of comments" do
-        # binding.pry
         expect(assigns(:comment)).to be_an_instance_of Comment
       end
       it "populates an array of group" do
@@ -30,14 +31,31 @@ require 'rails_helper'
     end
 
     describe 'POST #create' do
+      before do
+        user = create(:user)
+        login_user user
+        # controller_macros.rb内のlogin_userメソッドを呼び出し
+      end
       it "redirect to comments#index" do
         post :create, params: { group_id: group.id, comment: attributes_for(:comment)}
         expect(response).to redirect_to group_comments_path
       end
-    it "can save comment" do
-      expect {
-        post :create, params: { group_id: group.id, comment: attributes_for(:comment) }
-      }.to change(Comment, :count).by(1)
+
+      it "can save comment" do
+        expect {
+          post :create, params: { group_id: group.id, comment: attributes_for(:comment) }
+        }.to change(Comment, :count).by(1)
+      end
+
     end
+
+    describe 'POST #create' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it "renders the :index template" do
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
