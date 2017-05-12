8ed1c4783cd14bc4144a43f7ed1386274c5623b7
羽鳥　広大 <koudai.tori@icloud.com>
2017-05-12 13:56:54 +0900

    change to variable

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -18,9 +18,11 @@ require 'rails_helper'
       before do
         get :index, params: { group_id: group.id }
       end
+
       it "populates an array of comments" do
         expect(assigns(:comment)).to be_an_instance_of Comment
       end
+
       it "populates an array of group" do
         expect(assigns(:group)).to be_an_instance_of Group
       end
@@ -36,17 +38,21 @@ require 'rails_helper'
         login_user user
         # controller_macros.rb内のlogin_userメソッドを呼び出し
       end
+
+      subject {
+        Proc.new { post :create, params: { group_id: group.id, comment: attributes_for(:comment)} }
+      }
+
       it "redirect to comments#index" do
-        post :create, params: { group_id: group.id, comment: attributes_for(:comment)}
+        subject.call
         expect(response).to redirect_to group_comments_path
       end
 
       it "can save comment" do
         expect {
-          post :create, params: { group_id: group.id, comment: attributes_for(:comment) }
+          subject.call
         }.to change(Comment, :count).by(1)
       end
-
     end
 
     describe 'POST #create' do
