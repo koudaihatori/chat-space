c2b2b4d20de88a1a8f19139601e5fbb5c161d0a2
羽鳥　広大 <koudai.tori@icloud.com>
2017-05-19 19:11:11 +0900

    change js file

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,14 +1,15 @@
 $(function() {
   function buildHTML(comment) {
     var html =
-    `<div class="chat-comment__user">
+    `<div class="chat-comment">
+    <div class="chat-comment__user">
     ${comment.user_name}
     </div>
     <div class="chat-comment__time">
-    ${comment.created_at}
+    ${comment.reated_at.strftime('%Y年%m月%d日 %H:%M:%S')}
     <div class="chat-comment__text">
-    ${comment.created_at}
-    </div>`
+    ${comment.text}
+    </div>`;
     return html;
   }
   $('form_area').on('submit', function(e) {
@@ -20,14 +21,13 @@ $(function() {
       type: 'POST',
       url: '/comments.json',
       data: {
-        comments: {
+        comment: {
           text: comment
         }
       },
       dataType: 'json'
     })
     .done(function(data) {
-      console.log(data)
       var html = buildHTML(data);
       $('list').append(html);
       textField.val('');
