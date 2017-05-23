15365a9f326de5a3a6aefc9518c9c2678c07a5a9
羽鳥　広大 <koudai.tori@icloud.com>
2017-05-23 23:46:40 +0900

    add  continuous transmission

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -2,16 +2,19 @@ $(function() {
   function buildHTML(comment) {
     var html =
     `<div class="chat-comment">
-    <div class="chat-comment__user">
-    ${comment.user_name}
-    </div>
-    <div class="chat-comment__time">
-    ${comment.created_at}
-    <div class="chat-comment__text">
-    ${comment.text}
+      <div class="chat-comment__user">
+        ${comment.name}
+      </div>
+      <div class="chat-comment__time">
+        ${comment.created_at}
+      </div>
+      <div class="chat-comment__text">
+        ${comment.text}
+      </div>
     </div>`;
     return html;
   }
+
   $('.input_text_area').on('submit', function(e) {
     e.preventDefault();
     var textField = $('#text_field');
@@ -28,10 +31,10 @@ $(function() {
       dataType: 'json'
     })
     .done(function(data) {
-      // console.log(data)
-      var html = buildHTML(data.comment);
+      var html = buildHTML(data);
       $('#list').append(html);
       textField.val('');
+      $('input').prop("disabled", false);
     })
     .fail(function() {
       alert('error');
