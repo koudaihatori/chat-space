11424a76110793f0f46749828e08cde39f01c32f
羽鳥　広大 <koudai.tori@icloud.com>
2017-05-31 03:21:59 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
+  get '/users/search' => 'users#search'
   resources :groups, only: [:index, :new, :edit, :update, :create] do
     resources :comments, only: [:index, :create]
   end
