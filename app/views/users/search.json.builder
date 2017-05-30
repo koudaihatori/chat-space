34fc4ee26c99aee53786eca32293a140a2a170ff
羽鳥　広大 <koudai.tori@icloud.com>
2017-05-30 19:46:06 +0900

    delete file

--- app/views/users/search.json.builder
+++
@@ -1,4 +0,0 @@
-json.users @users.each do |user|
-  json.id user.id
-  json.name user.name
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  get '/users' => 'users#search'
   resources :groups, only: [:index, :new, :edit, :update, :create] do
     resources :comments, only: [:index, :create]
   end
